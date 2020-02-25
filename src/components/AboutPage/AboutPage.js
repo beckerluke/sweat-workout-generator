9e3752fdb002914199978c791e81b0552c73f336
Luke Becker <beckerluke1@gmail.com>
2020-02-24 20:01:44 -0600

    initial commit

---
+++ src/components/AboutPage/AboutPage.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import mapStoreToProps from '../../redux/mapStoreToProps';
+// This is one of our simplest components
+// It doesn't have local state, so it can be a function component.
+// It doesn't dispatch any redux actions or display any part of redux state
+// or even care what the redux state is, so it doesn't need 'connect()'
+
+class ExerciseInput extends Component {
+  
+  render() {
+    
+    return(
+      <div>
+        <div>
+          <p>
+            This about page is for anyone to read!
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default connect(mapStoreToProps)(ExerciseInput);
