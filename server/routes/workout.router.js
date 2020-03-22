50ca2c444606f48a76aa98ac8ab53ee0e182d9f7
Luke Becker <beckerluke1@gmail.com>
2020-03-22 14:10:50 -0500

    Simplified query in workout router for fetching random total body workout exercises for each body part

--- server/routes/workout.router.js
+++ server/routes/workout.router.js
@@ -2,38 +2,21 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 const BODY_PART_ARRAY = require('../strategies/BodyPart');
+
 /**
  * GET TOTAL BODY WORKOUT
  */
 router.get('/total/body', (req, res) => {
-
-    const queryText = `SELECT * FROM  "back_exercises", "chest_exercises", 
-                        "shoulder_exercises", "quad_exercises", "hamstring_exercises", 
-                        "core_exercises", "biceps_exercises", "triceps_exercises"
-                        ORDER BY RANDOM()
-                        LIMIT 1;`;
+    // query to get a random row for each body part type
+    const queryText = `SELECT DISTINCT ON ("type") *
+                        FROM "exercise"
+                        ORDER BY "type", random();`;
 
     pool.query(queryText).then(result => {
         // Sends back the results in an object
         console.log('result.rows: ', result.rows);
-        // convert object of exercises into array for loop possibility
-        const selectedExercises = Object.entries(result.rows[0]);
-        // remove unnecessary id
-        const itemToRemove = selectedExercises.shift();
-        const exercisesArray = selectedExercises.filter(item => item !== itemToRemove);
-        /* convert exercise data from database into an organized array of
-        objects according to body part exercise name and description */
-        const convertedExercisesArray = [];
-        for (let i = 0; i < exercisesArray.length; i+=2) {
-            let entries = new Map([
-                exercisesArray[i],
-                exercisesArray[i+1],
-            ]);
-            convertedExercisesArray.push(Object.fromEntries(entries));
-        }
-        console.log('convertedExercises: ', convertedExercisesArray)
-        
-        res.send(convertedExercisesArray);
+        // send random total body exercises
+        res.send(result.rows);
     })
     .catch(error => {
         console.log('error getting exercises', error);
