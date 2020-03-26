2bf1c2723306045e2df9a7c3e3a6045c02fac84f
Luke Becker <beckerluke1@gmail.com>
2020-03-26 16:45:14 -0500

    Cleaned up database.sql to more up-to-date database setup

--- database.sql
+++ database.sql
@@ -9,76 +9,9 @@ CREATE TABLE "user" (
     "password" VARCHAR (1000) NOT NULL
 );
 
-CREATE TABLE "workout_style" (
-	"id" SERIAL PRIMARY KEY,
-	"reps" INTEGER,
-	"sets" INTEGER,
-	"time" TIME
-);
-
 CREATE TABLE "exercise" (
     "id" SERIAL PRIMARY KEY,
     "name" VARCHAR (200) UNIQUE NOT NULL,
     "description" VARCHAR (2000),
     "type" VARCHAR (200)
 );
-
--- EXERCISES BY BODY PART
-CREATE TABLE "back_exercises" (
-    "id" SERIAL PRIMARY KEY,
-    "back_exercises_name" VARCHAR (200) UNIQUE NOT NULL,
-    "back_exercises_description" VARCHAR (2000)
-);
-
-CREATE TABLE "chest_exercises" (
-    "id" SERIAL PRIMARY KEY,
-    "chest_exercises_name" VARCHAR (200) UNIQUE NOT NULL,
-    "chest_exercises_description" VARCHAR (2000)
-);
-
-CREATE TABLE "shoulder_exercises" (
-    "id" SERIAL PRIMARY KEY,
-    "shoulder_exercises_name" VARCHAR (200) UNIQUE NOT NULL,
-    "shoulder_exercises_description" VARCHAR (2000)
-);
-
-CREATE TABLE "quad_exercises" (
-    "id" SERIAL PRIMARY KEY,
-    "quad_exercises_name" VARCHAR (200) UNIQUE NOT NULL,
-    "quad_exercises_description" VARCHAR (2000)
-);
-
-CREATE TABLE "hamstring_exercises" (
-    "id" SERIAL PRIMARY KEY,
-    "hamstring_exercises_name" VARCHAR (200) UNIQUE NOT NULL,
-    "hamstring_exercises_description" VARCHAR (2000)
-);
-
-CREATE TABLE "core_exercises" (
-    "id" SERIAL PRIMARY KEY,
-    "core_exercises_name" VARCHAR (200) UNIQUE NOT NULL,
-    "core_exercises_description" VARCHAR (2000)
-);
-
-CREATE TABLE "triceps_exercises" (
-    "id" SERIAL PRIMARY KEY,
-    "triceps_exercises_name" VARCHAR (200) UNIQUE NOT NULL,
-    "triceps_exercises_description" VARCHAR (2000)
-);
-
-CREATE TABLE "biceps_exercises" (
-    "id" SERIAL PRIMARY KEY,
-    "biceps_exercises_name" VARCHAR (200) UNIQUE NOT NULL,
-    "biceps_exercises_description" VARCHAR (2000)
-);
--- END OF EXERCISES BY BODY PART
-
-CREATE TABLE "hiit_exercises" (
-    "id" SERIAL PRIMARY KEY,
-    "hiit_exercises_name" VARCHAR (200) UNIQUE NOT NULL,
-    "hiit_exercises_description" VARCHAR (2000)
-);
--- CREATE TABLE "workout_log" (
---     "id" PRIMARY SERIAL KEY,
---     "date" timestamp,
--- )
